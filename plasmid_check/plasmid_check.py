450b910606c337cdfed8bccc6719f5275e9fde07
Chad Chiang <cchiang@hpc-sandbox.neb.com>
2026-07-20 21:26:54 -0400

    Remove plot function

--- plasmid_check/plasmid_check.py
+++ plasmid_check/plasmid_check.py
@@ -209,14 +209,12 @@ def compare_sequences(
 
     return mismatches
 
-def check_sanger_sequence(
+
+def check_plasmid_sequence(
     dna_seq_name,
     dna_seq,
-    sanger_seq_name,
-    sanger_seq,
-    plot=False,
-    plot_outfile=None,
-    plot_width=10,
+    read_seq_name,
+    read_seq,
     gap_penalty=-10,
     target_start=None,
     target_end=None,
@@ -224,11 +222,11 @@ def check_sanger_sequence(
     """Compare whole plasmid sequencing reads to reference template."""
 
     template_len = len(dna_seq)
-    sample_len = len(sanger_seq)
+    sample_len = len(read_seq)
 
     # Print length information
     print(f"-> Template ({dna_seq_name}) Length: {template_len} bp")
-    print(f"-> Sample ({sanger_seq_name}) Length: {sample_len} bp")
+    print(f"-> Sample ({read_seq_name}) Length: {sample_len} bp")
 
     # --- EARLY EXIT IF LENGTHS DO NOT MATCH ---
     if template_len != sample_len:
@@ -241,7 +239,7 @@ def check_sanger_sequence(
         return pd.DataFrame(
             columns=[
                 "dna_seq_name",
-                "sanger_seq_name",
+                "read_seq_name",
                 "region",
                 "dna_pos",
                 "ref_seq",
@@ -255,24 +253,24 @@ def check_sanger_sequence(
     # --- IF LENGTHS MATCH, RUN ALIGNMENT ---
     print("-> Result: Lengths match perfectly!")
 
-    ali = pairwise_dna(dna_seq, sanger_seq, gap_penalty=gap_penalty)
+    ali = pairwise_dna(dna_seq, read_seq, gap_penalty=gap_penalty)
 
-    sanger_dna_start = ali.trace[0][0]
-    sanger_dna_end = ali.trace[-1][0]
+    read_dna_start = ali.trace[0][0]
+    read_dna_end = ali.trace[-1][0]
 
     mismatches = compare_sequences(
         dna_seq,
-        sanger_seq,
+        read_seq,
         ali,
         target_start=target_start,
         target_end=target_end,
     )
 
-    print(f"{dna_seq_name}, {sanger_seq_name}:")
+    print(f"{dna_seq_name}, {read_seq_name}:")
     print(f"\tdna_seq: {dna_seq}")
-    print(f"\tsanger_seq: {sanger_seq}")
+    print(f"\tread_seq: {read_seq}")
     print(
-        f"\tAlignment Nucleotides: {sanger_dna_start+1} to {sanger_dna_end+1}"
+        f"\tAlignment Nucleotides: {read_dna_start+1} to {read_dna_end+1}"
     )
     print(f"\tMismatches:")
 
@@ -298,51 +296,12 @@ def check_sanger_sequence(
     else:
         print("\tNone")
 
-    if plot:
-        aln_len = len(ali.trace)
-        plot_height = max(int(plot_width * aln_len / 400), plot_width)
-        fig, axs = plt.subplots(
-            nrows=1, ncols=1, figsize=(plot_height, plot_width)
-        )
-        plt.suptitle(f"{dna_seq_name}\n{sanger_seq_name}")
-
-        std_matrix = bioalign.SubstitutionMatrix.std_nucleotide_matrix()
-        alphabet_1 = std_matrix.get_alphabet1()
-        alphabet_2 = std_matrix.get_alphabet2()
-        # FIX: Explicitly specify integer dtype
-        score_matrix = np.ones(
-            (len(alphabet_1), len(alphabet_2)), dtype=np.int32
-        )
-
-        for i in range(4):
-            for j in range(4):
-                score_matrix[i, j] = 10
-        for i in range(len(alphabet_1)):
-            score_matrix[i, i] = 0
-
-        matrix = bioalign.SubstitutionMatrix(
-            alphabet_1, alphabet_2, score_matrix
-        )
-        graphics.plot_alignment_similarity_based(
-            axs,
-            ali,
-            matrix=matrix,
-            labels=["DNA Sequence", "Sequencing Read"],
-            show_numbers=True,
-            show_line_position=True,
-        )
-        plt.tight_layout()
-        if plot_outfile:
-            plt.savefig(plot_outfile, dpi=300)
-        plt.show()
-        plt.close()
-
     df_rows = []
     for m in mismatches:
         df_rows.append(
             [
                 dna_seq_name,
-                sanger_seq_name,
+                read_seq_name,
                 m.region,
                 m.seq1_pos + 1,
                 m.ref_nt,
@@ -357,7 +316,7 @@ def check_sanger_sequence(
         df_rows,
         columns=[
             "dna_seq_name",
-            "sanger_seq_name",
+            "read_seq_name",
             "region",
             "dna_pos",
             "ref_seq",
